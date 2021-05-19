f114b128d760c39a3b2d5de125be8da154e725a7
Rodrigo Baliani <rodro96@gmail.com>
2021-05-18 22:13:41 -0300

    Login con Google

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,11 +7,19 @@ const Login = ({ navigation }) => {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const { signIn } = useAuth();
+    const { signIn, signInWithGoogle } = useAuth();
 
     async function signInUser(email, password) {
         try {
             await signIn(email, password)
+        } catch (error) {
+            alert(error.message)
+        }
+    }
+
+    async function signInUserWithGoogle() {
+        try {
+            await signInWithGoogle();
         } catch (error) {
             alert(error.message)
         }
@@ -24,6 +32,7 @@ const Login = ({ navigation }) => {
             <TextInput placeholder='Email' onChangeText={email => setEmail(email)} />
             <TextInput secureTextEntry placeholder='Contraseña' onChangeText={password => setPassword(password)} />
             <Button title="Iniciar Sesión" onPress={() => signInUser(email, password)} />
+            <Button title="Iniciar Sesión con Google" onPress={() => signInUserWithGoogle()} />
             <Text>¿No tiene cuenta?</Text>
             <Button title="Registrarse" onPress={() => navigation.navigate('Signup')} />
         </View >
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,5 +1,8 @@
 import React, { useContext, useState, useEffect } from "react"
 import auth from '@react-native-firebase/auth';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+
+
 
 const AuthContext = React.createContext()
 
@@ -7,7 +10,13 @@ export function useAuth() {
     return useContext(AuthContext)
 }
 
+GoogleSignin.configure({
+    webClientId: '841200999064-1dqu7ut22543otu9qd3lj897aev8jfq1.apps.googleusercontent.com'
+});
+
 export function AuthProvider({ children }) {
+
+
 
     const [currentUser, setCurrentUser] = useState()
 
@@ -23,12 +32,24 @@ export function AuthProvider({ children }) {
         return auth().signOut();
     }
 
+    async function signInWithGoogle() {
+        // Get the users ID token
+        const { idToken } = await GoogleSignin.signIn();
+
+        // Create a Google credential with the token
+        const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+        // Sign-in the user with the credential
+        return auth().signInWithCredential(googleCredential);
+    }
+
     const value = {
         currentUser,
         setCurrentUser,
         signUp,
         signIn,
-        signOut
+        signOut,
+        signInWithGoogle
     }
     return (
         <AuthContext.Provider value={value}>
